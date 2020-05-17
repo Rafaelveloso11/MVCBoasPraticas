e304fdfdbeba0fb4878185ebe5b9668292eedaa9
Rafael Veloso <rafaelveloso11@gmail.com>
2020-05-17 14:07:26 -0300

    Criação das views

--- MVCBoasPraticas/Controllers/DefaultController.cs
+++ MVCBoasPraticas/Controllers/DefaultController.cs
@@ -6,15 +6,10 @@ using System.Web.Mvc;
 
 namespace MVCBoasPraticas.Controllers
 {
-    public class HomeController : Controller
+    public class DefaultController : Controller
     {
-        // GET: Home
+        // GET: Default
         public ActionResult Index()
-        {
-            return View();
-        }
-
-        public ActionResult Contatos()
         {
             return View();
         }
